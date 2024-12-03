b113de80a7356a89c19a9faae3f24025ea3fb9e3
MathieuAudibert <mathieu.audibert27@gmail.com>
2024-12-03 12:51:01 +0100

    bdd + detail

--- src/detail/view.php
+++ src/detail/view.php
@@ -12,8 +12,13 @@ function view_detail(): void
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Trackable | Detail du colis</title>
-
+        <link rel="stylesheet" href="public/header.css">
         <link rel="stylesheet" href="public/detail/style.css">
+        <div id="header">
+            <a href="/index.php"><img src="public\images\truck-svgrepo-com.svg" alt="logo" class="logo"></a>
+            <a href="/register"><button class="button">S'inscrire</button></a>
+        </div>
+        <hr>
     </head>
 
     </html>
