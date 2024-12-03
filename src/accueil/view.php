ddf60615c69c8e0161dbbd5aec2e98fa6ace23ad
MathieuAudibert <mathieu.audibert27@gmail.com>
2024-12-03 09:52:15 +0100

    ajout header

--- src/accueil/view.php
+++ src/accueil/view.php
@@ -12,8 +12,13 @@ function view_accueil(): void
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Trackable | Accueil</title>
-
+        <link rel="stylesheet" href="public/header.css">
         <link rel="stylesheet" href="public/accueil/style.css">
+        <div id="header">
+            <a href="/index.php"><img src="public\images\truck-svgrepo-com.svg" alt="logo" class="logo"></a>
+            <a href="/register"><button class="button">S'inscrire</button></a>
+        </div>
+        <hr>
     </head>
 
     </html>
