83a109a818af7283fc8828134fd7c877923c1f65
MathieuAudibert <mathieu.audibert27@gmail.com>
2024-12-06 09:21:30 +0100

    correction de mauvais id .env + debut de la requete de register + view register login faites par @SB9497

--- src/register/model.php
+++ src/register/model.php
@@ -7,7 +7,11 @@ require_once dirname(__DIR__, 2) . '/utils/bdd.php';
 function model_register(): void
 {
   try {
-
+    $db = Database::getConnection();
+    $stmt = $db->prepare("INSERT INTO users (email, mdp, role_user");
+    $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+    $stmt->execute();
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
     } catch (Exception $e) {
         error_log($e->getMessage(), 3, '../../utils/logs/errors.log');
         echo "Erreur :" + $e->getMessage();
